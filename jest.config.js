04d7caf9d04a7f6edf1f63325db45ac301c7444a
NaoyaMiyagawa <tsuna2764@gmail.com>
2021-08-29 20:28:02 +0900

    test(jest): surpress vue script setup and element plus warning

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,15 @@
 module.exports = {
+  // verbose: true,
   preset: 'ts-jest',
   globals: {},
   testEnvironment: 'jsdom',
   transform: {
     '^.+\\.vue$': 'vue-jest',
-    '^.+\\js$': 'babel-jest',
+    '^.+\\ts$': 'ts-jest',
   },
   moduleNameMapper: {
     '^@/(.+)$': '<rootDir>/src/$1',
   },
   moduleFileExtensions: ['vue', 'js', 'json', 'jsx', 'ts', 'tsx', 'node'],
+  testMatch: ['<rootDir>/tests/**/*.ts'],
 };
