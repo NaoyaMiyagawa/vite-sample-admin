d8bcd3a47dd9b56d787afbad170a3e933fda84bd
NaoyaMiyagawa <tsuna2764@gmail.com>
2021-09-01 23:23:12 +0900

    feat(sidebar): add routing

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,15 +1,25 @@
 <script setup lang="ts">
 import { System } from '@/consts/system';
 import { Notification } from '@element-plus/icons';
+import { computed } from '@vue/reactivity';
+import { useRoute } from 'vue-router';
 
 defineProps({
   isCollapsed: { type: Boolean, required: true },
 });
 defineEmits(['toggle-side-bar']);
 
+const route = useRoute();
+
+const { headerTitle } = (() => {
+  const pageName = computed(() => route.name);
+  const headerTitle = computed(() => `${System.name} - ${pageName.value}`);
+  return { headerTitle };
+})();
+
 const loginUserName = 'Jedrich';
-const pageName = 'Articles CMS';
 const circleUrl = 'https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png';
+
 const activeIndex = '1';
 </script>
 
@@ -20,7 +30,7 @@ const activeIndex = '1';
         <i :class="[isCollapsed ? 'el-icon-s-unfold' : 'el-icon-s-fold']" />
       </el-menu-item>
 
-      <div class="m-auto ml-4">{{ System.name }} - {{ pageName }}</div>
+      <div class="m-auto ml-4">{{ headerTitle }}</div>
     </div>
 
     <div class="flex-grow"></div>
